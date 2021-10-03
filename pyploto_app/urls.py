c91a3481b8cb8b39045199cb977ba5e3bcd2fddb
tvergara <tverg419@gmail.com>
2021-10-03 07:53:32 -0400

    added UserDetail url

--- pyploto_app/urls.py
+++ pyploto_app/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('blacklist/',     views.LogoutUser.as_view(),              name='token-blacklist'),
     # User paths
     path('users/',              views.UserList.as_view(),   name='user-list'),
-    path('users/<int:id>', views.UserDetail.as_view(), name='post-detail'),
+    path('users/<int:id>',      views.UserDetail.as_view(), name='user-detail'),
     path('users/create/',       views.UserCreate.as_view(), name='user-create'),
     # Posts paths
     path('posts/',              views.PostListCreate.as_view(),            name='post-list'),
